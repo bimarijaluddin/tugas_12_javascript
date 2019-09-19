1dd8e7e5d84806bac06fc928569f16e4597402a5
bima rijaluddin <bimarijal@gmail.com>
2019-09-19 11:47:39 +0800

    Tugas 12 javascript
    Tugas dari niomic

---
+++ tugas12.js
@@ -0,0 +1,14 @@
+function namaBuah(){
+    var buah = ['pisang','jeruk','apel','mangga'];
+    console.log(buah);
+
+    buah.pop()
+    console.log(buah);
+
+    buah.shift()
+    console.log(buah);
+
+    return buah
+}
+
+(namaBuah());
